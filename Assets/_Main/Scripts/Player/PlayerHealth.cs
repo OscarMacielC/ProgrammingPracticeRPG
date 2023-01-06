c41b92d1662177c0707ee7424ba89ca674b3235c
Oscar Maciel Castillo <oscarmacielc@live.com.mx>
2023-01-06 15:09:03 -0600

    Scripts PlayerHealth and UI
    Made the scripts for the health control
    109 more files were changed
    
    The important ones are PlayerHealthUIManager and PlayerHealth

---
+++ Assets/_Main/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,155 @@
+using UnityEngine;
+
+/// <summary>
+/// --Player Health--<para></para>
+/// 
+/// This script controls the health of the player, their actual and max number of hearts
+/// Also has the upper limit for the number of hearts
+/// It has methods to take damage, heal and increase max hp.
+/// </summary>
+/// 
+public class PlayerHealth : MonoBehaviour
+{
+    #region FIELDS
+
+    const int LIFETORESTORE = 1;
+    const int LIFETOINCREASE = 1;
+    const int DAMAGETOTAKE = 1;
+    const int MINHEALTH = 0;
+
+    [Header("Health")]
+    [Tooltip("Upper limit for health or heart containers.")]
+    [SerializeField] private int _maxHPLimit;
+    [Tooltip("Maximum health or heart containers.")]
+    [SerializeField] private int _maxHealth;
+    [Tooltip("Actual health or heart containers.")]
+    [SerializeField] private int _health;
+
+
+    [Header("Heal on MaxHP upgrade")]
+    [Tooltip("Heal a heart when MaxHP should increase.")]
+    [SerializeField] private bool _healOnMaxHpUp;
+
+    private static PlayerHealth _instance;
+
+    #endregion
+
+    #region PROPERTIES
+    /// <summary> Instance of the script for singleton use. </summary>
+    public static PlayerHealth instance
+    {
+        get { return _instance; }
+    }
+    /// <summary> Upper HP limit. </summary>
+    public int maxHPLimit
+    {
+        get { return _maxHPLimit; }
+        set { _maxHPLimit = value; }
+    }
+    /// <summary> Maximum HP. </summary>
+    public int maxHealth
+    {
+        get { return _maxHealth; }
+        set { _maxHealth = value; }
+    }
+    /// <summary> Actual HP. </summary>
+    public int health
+    {
+        get { return _health; }
+        set { _health = value; }
+    }
+    /// <summary> If player recovers HP when maxHP increases. </summary>
+    public bool healOnMaxHpUp
+    {
+        get { return _healOnMaxHpUp; }
+        set { _healOnMaxHpUp = value; }
+    }
+
+    /// <summary> Delegate to call when need to update hearts. </summary>
+    public delegate void UpdateHearts();
+    /// <summary> Delegate to call when need to add hearts. </summary>
+    public delegate void AddHearts();
+
+    /// <summary> Event when player damage is taken. </summary>
+    public static event UpdateHearts OnDamageTaken;
+    /// <summary> Event when player hp is restored. </summary>
+    public static event UpdateHearts OnRestoreHP;
+    /// <summary> Event when player max hp is increased. </summary>
+    public static event AddHearts OnMaxHealthIncreased;
+
+    #endregion
+
+    #region METHODS
+    public void TakeDamage()
+    {
+        if (_health <= MINHEALTH)
+        {
+            return;
+        }
+        _health -= DAMAGETOTAKE;
+
+        if (OnDamageTaken != null)
+        {
+            OnDamageTaken.Invoke();
+        }
+    }
+
+    public void Heal()
+    {
+        if (_health >= _maxHealth)
+        {
+            return;
+        }
+        _health += LIFETORESTORE;
+
+
+        if (OnRestoreHP != null)
+        {
+            OnRestoreHP.Invoke();
+        }
+    }
+
+    public void UpgradeHealth()
+    {
+        if (_maxHealth >= _maxHPLimit)
+        {
+            if (_healOnMaxHpUp)
+            {
+                Heal();
+            }
+            return;
+        }
+        _maxHealth += LIFETOINCREASE; // "IVAN" Usar ++ o dejarlo para un objeto especial
+
+        if (_healOnMaxHpUp) // "IVAN" Se usa arriba ¿Hacer función?
+        {
+            Heal();
+        }
+
+        if (OnMaxHealthIncreased != null)
+        {
+            OnMaxHealthIncreased.Invoke();
+        }
+    }
+
+    #endregion
+
+    #region MONOBEHAVIOUR
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            return;
+        }
+        Destroy(gameObject); // "IVAN" ¿Es mejor destruir script?
+    }
+
+    private void Start()
+    {
+        //_health = _maxHealth;
+    }
+
+    #endregion
+}
---
+++ Assets/_Main/Scripts/UI/PlayerHealthUIManager.cs
@@ -0,0 +1,145 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+/// <summary>
+/// --Health UI Manager--<para></para>
+/// 
+/// This script controls the UI for the health of the player
+/// Also has the upper limit for the number of hearts
+/// Its subcribed to the player events and updates the UI accordingly
+/// </summary>
+/// <remarks> Execution order should be after PlayerHealth. </remarks>
+/// 
+public class PlayerHealthUIManager : MonoBehaviour
+{
+    #region FIELDS
+
+    [Header("Player Data")]
+    [Tooltip("Singleton PlayerHealth script")]
+    [SerializeField] private PlayerHealth _playerHealth;
+
+    [Header("Hearts")]
+    [Tooltip("Gameobject prefab with the image of the full heart")]
+    [SerializeField] private GameObject _heart;
+    [SerializeField] private Image _heartImage;
+    [Tooltip("Gameobject prefab with the image of the hollow heart")]
+    [SerializeField] private GameObject _hollowHeart;
+    [SerializeField] private Image _hollowHeartImage;
+
+    private struct HeartUI // "Ivan" Como se nombran los structs
+    {
+        public GameObject _heartGO;
+        public Image _heartIMG;
+        public Animator _heartAnim;
+    }
+
+    private class HeartDictionary : Dictionary<int, HeartUI> // "Ivan" Como se nombran los diccionaries
+    {
+        private HeartUI newHeart;
+        internal void Add(int key, GameObject heartGO)
+        {
+            newHeart._heartGO = heartGO;
+            newHeart._heartIMG = heartGO.GetComponent<Image>();
+            newHeart._heartAnim = heartGO.GetComponent<Animator>();
+            this.Add(key, newHeart);
+        }
+    }
+
+    HeartDictionary _heartDictionary;
+
+    #endregion
+
+    #region METHODS
+
+    /*
+    private void UpdateHeartUI()
+    {
+        // Podría revisar vida máxima, vida actual y poner los corazones
+    }*/
+
+    void TakeDamage() // "Ivan" En lugar de hacer todo esto es mejor un update heartsUI que revise y los haga todos???
+    {
+        _heartDictionary[_playerHealth.health]._heartAnim.enabled = false;
+        _heartDictionary[_playerHealth.health]._heartIMG.sprite = _hollowHeartImage.sprite;
+    }
+
+    void HealHearts()
+    {
+        int HeartToChange = _playerHealth.health - 1;
+        _heartDictionary[HeartToChange]._heartAnim.enabled = true;
+    }
+
+    void AddHearts()
+    {
+        int HeartToChange = _playerHealth.maxHealth - 1; // "Ivan" Usar constante del player?? crear nueva o hacerla variable para no usar -1 ni --
+
+        _heartDictionary[HeartToChange]._heartGO.SetActive(true);
+        _heartDictionary[HeartToChange]._heartAnim.enabled = false;
+        _heartDictionary[HeartToChange]._heartIMG.sprite = _hollowHeartImage.sprite;
+
+        if (_playerHealth.maxHealth >= _playerHealth.maxHPLimit)
+        {
+            if (_playerHealth.healOnMaxHpUp)
+            {
+                HealHearts();
+            }
+            return;
+        }
+
+        if (_playerHealth.healOnMaxHpUp) // "IVAN" Se usa arriba ¿Hacer función?
+        {
+            HealHearts();
+        }
+
+
+    }
+
+    #endregion
+
+    #region MONOBEHAVIOUR
+
+    private void OnEnable()
+    {
+        PlayerHealth.OnDamageTaken += TakeDamage;
+        PlayerHealth.OnRestoreHP += HealHearts;
+        PlayerHealth.OnMaxHealthIncreased += AddHearts;
+    }
+
+    private void OnDisable()
+    {
+        PlayerHealth.OnDamageTaken -= TakeDamage;
+        PlayerHealth.OnRestoreHP -= HealHearts;
+        PlayerHealth.OnMaxHealthIncreased -= AddHearts;
+    }
+
+    private void Reset()
+    {
+        _playerHealth = FindObjectOfType<PlayerHealth>();
+    }
+
+    void Start()
+    {
+        _heartImage = _heart.GetComponent<Image>();
+        _hollowHeartImage = _hollowHeart.GetComponent<Image>();
+
+        for (int i = default; i < _playerHealth.maxHPLimit; i++)
+        {
+            GameObject heart = Instantiate(_heart, this.transform);
+            _heartDictionary.Add(i, heart);
+            _heartDictionary[i]._heartGO.SetActive(false);
+            _heartDictionary[i]._heartAnim.enabled = false;
+        }
+        for (int i = default; i < _playerHealth.maxHealth; i++)
+        {
+            _heartDictionary[i]._heartGO.SetActive(true);
+        }
+        for (int i = default; i < _playerHealth.health; i++)
+        {
+            _heartDictionary[i]._heartAnim.enabled = true;
+        }
+    }
+
+    #endregion
+}
